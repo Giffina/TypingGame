ffb9d5fcddcf8c8aa77aab94b1fafdebdfe2b535
Giffina <46724077+Giffina@users.noreply.github.com>
2019-02-22 02:40:25 -0600

    Final commit
    There is a beginning screen, end screen with final score, a score system, it will end if a word touches the bottom, there is a tone that plays on start and a tone that plays when a word is destroyed, also it will loop through hello world until the user loses.

--- Assets/Scripts/WordGenerator.cs
+++ Assets/Scripts/WordGenerator.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class WordGenerator : MonoBehaviour {
-
-    private static string[] wordList = { "sidewalk", "robin", "three", "protect", "periodic",
+    static int index = -1;
+    /*private static string[] wordList = { "sidewalk", "robin", "three", "protect", "periodic",
                                     "somber", "majestic", "jump", "pretty", "wound", "jazzy",
                                     "memory", "join", "crack", "grade", "boot", "cloudy", "sick",
                                     "mug", "hot", "tart", "dangerous", "mother", "rustic", "economic",
@@ -18,13 +18,29 @@ public class WordGenerator : MonoBehaviour {
                                     "sin", "box", "pies", "awesome", "root", "available", "sleet", "wax",
                                     "boring", "smash", "anger", "tasty", "spare", "tray", "daffy", "scarce",
                                     "account", "spot", "thought", "distinct", "nimble", "practise", "cream",
-                                    "ablaze", "thoughtless", "love", "verdict", "giant"};
+                                    "ablaze", "thoughtless", "love", "verdict", "giant"};*/
 
-    public static string GetRandomWord()
+    static TextAsset words = Resources.Load<TextAsset>("helloWorld");
+    private static string[] wordList = words.text.Split(new char[] { ' ' });
+
+    public static string ReadWords()
+    {
+        if (index < wordList.Length)
+            index = index + 1;
+        else if (index == wordList.Length)
+            index = -1;
+        else
+            index = index + 1;
+        string wordsRead = wordList[index];
+
+        return wordsRead;
+    }
+
+    /*public static string GetRandomWord()
     {
         int randomIndex = Random.Range(0, wordList.Length);
         string randomWord = wordList[randomIndex];
 
         return randomWord;
-    }
+    }*/
 }
